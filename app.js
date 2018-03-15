61ed57c8570032dd9db0703b7be4008d5b7c4f90
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-15 13:34:42 +1100

    finalized routing excercise

--- app.js
+++ app.js
@@ -7,23 +7,46 @@ app.get('/', (req, res) => {
 });
 
 // Dynamic routing for animal noises
-app.get('speak/:animal', (req, res) => {
-  const animal = req.param();
+app.get('/speak/:animal', (req, res) => {
+  // Const animal = req.params.animal;
+  const {animal} = req.params;
   let sound = 0;
 
-  if (animal.toLocaleLowerCase() === 'pig') {
+  if (animal === 'pig') {
     sound = 'Oink';
-  } else if (animal.toLocaleLowerCase() === 'dog') {
+  } else if (animal === 'dog') {
     sound = 'woof';
-  } else if (animal.toLocaleLowerCase() === 'cat') {
+  } else if (animal === 'cat') {
     sound = 'meow';
-  } else if (animal.toLocaleLowerCase() === 'cow') {
+  } else if (animal === 'cow') {
     sound = 'mooooh';
-  } else if (animal.toLocaleLowerCase() === 'horse') {
-    sound = 'Neigh';
+  } else if (animal === 'horse') {
+    sound = 'neigh';
   } else {
     sound = '{insert animal sound}';
   }
 
   res.send(`The ${animal} says '${sound}'`);
+});
+
+app.get('/repeat/:word/:count', (req, res) => {
+  const {word} = req.params;
+  const {count} = req.params;
+  let str = '';
+
+  for (let counter = 0; counter < count; counter++) {
+    str += ` ${word}`;
+  }
+
+  res.send(str);
+});
+
+app.get('*', (req, res) => {
+  res.send('Sorry, page not found...what are you doing with your life?');
+});
+
+app.set('case sensitive routing', false);
+// Server listen on port settings
+app.listen(3000, () => {
+  console.log('Server started listening on port 3000');
 });
